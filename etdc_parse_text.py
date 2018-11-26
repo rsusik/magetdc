714a6aa96ba2c5e6f137828c6b253e61b8f6117d
rsusik <robert.susik@gmail.com>
2018-11-26 14:09:50 +0000

    python scripts update

--- etdc_parse_text.py
+++ etdc_parse_text.py
@@ -1,17 +1,21 @@
-import sys
+import sys, argparse
 
-set_loc = '/path/to/sets'
+set_loc = './sets'
 
-t_args = sys.argv[1]
-t_args_list	= t_args.split(',')     # text files separated by ','
+parser = argparse.ArgumentParser(description='Corpus preprocessing script for ETDC MAG variant.',
+			epilog="Example:\npython3 etdc_parse_text.py -c english.10MB")
+parser.add_argument("-c", "--corpus", dest='c', type=str, default='english.10MB', help="corpus")
+args = parser.parse_args()
+
+t_args_list = args.c.split(',')	 # text files separated by ','
 
 for t_el in t_args_list:
 	filename = set_loc + '/' + t_el
-	f_in = open(filename)
-	text = f_in.read()
-	text = text.replace(",", " ").replace("\r", " ").replace("\n", " ").replace("\t", " ").replace(".", " . ")
-	while text.find("  ") != -1:
-		text = text.replace("  ", " ")
-	f_out = open(filename + ".processed", "w+")
-	f_out.write(text)
-	f_out.close()
+	with open(filename, "r", encoding="utf8") as f_in, open(filename + ".processed", "w+") as f_out:
+		text = f_in.read()
+		text = text.replace(",", " ").replace("\r", " ").replace("\n", " ").replace("\t", " ").replace(".", " . ")
+		while text.find("  ") != -1:
+			text = text.replace("  ", " ")
+		f_out.write(text)
+
+
--- etdc_test.py
+++ etdc_test.py
@@ -1,78 +1,37 @@
-import subprocess, sys, os, platform
+import subprocess, sys, os, platform, argparse
 
-set_loc = '/path/to/sets'
-alg_loc = '/path/to/algs'
+set_loc = '/sets'
+alg_loc = '.'
 
-args_num = len(sys.argv) - 1
 curr_path = os.getcwd()
 
-arg_offset = 0
 
-# Error
-if args_num < 1:
-    print >> sys.stderr, "Error not enought parameters. See --help."
-    exit()
+parser = argparse.ArgumentParser(description='MAG testing script.',
+                epilog="Example:\npython3 etdc_test.py -a etdc_mag -c english.10MB -r 100 -m 8,32 -q 2 -s 5 -u 4 -k 2 -d 1000000")
+parser.add_argument("-r", "--npatterns", dest='r', type=str, default='100', help="number of patterns")
+parser.add_argument("-a", "--algorithm", dest='a', type=str, default='etdc_mag_l2', help="algorithm[s] to be tested")
+parser.add_argument("-c", "--corpus", dest='c', type=str, default='english.10MB', help="corpus")
+parser.add_argument("-m", "--length", dest='m', type=str, default='8,16,32,64', help="pattern length[s] (e.g. 8,16,32)")
+parser.add_argument("-u", "--faosou", dest='u', type=str, default='4', help="FAOSO parameter U")
+parser.add_argument("-k", "--faosok", dest='k', type=str, default='2', help="FAOSO parameter k")
+parser.add_argument("-q", "--q-gram", dest='q', type=str, default='2', help="q-gram size")
+parser.add_argument("-s", "--sigma", dest='s', type=str, default='4', help="dest. alph. size")
+parser.add_argument("-d", "--denom", dest='d', type=str, default='1000000', help="denominator (e.g. 100,1000)")
 
-if sys.argv[1]=='--help':
-	print 'Pattern matching tester help.'
-	print ''
-	print 'Syntax:'
-	print 'test [--help] algorithm[.exe] test_set r m u k q-size sigma'
-	print ''
-	print '--help      Shows the help'
-	print 'algorithm   Algorithm(eg. cf,fso)'
-	print 'test_set    Test set(eg. english.100MB)'
-	print 'r           Pattern number(eg. 1000)'
-	print 'm           Pattern length(eg. 8)'
-	print 'u           U parameter(eg. 8)'
-	print 'k           K parameter(eg. 2)'
-	print 'q           q-gram size'
-	print 'sig         alph size'
-	print 'denom       denom'
-	print ''
-	print 'Example:'
-	print 'etdc_test.py etdc_mag.exe english.200MB 100,1000 8,16,32,64 8 1,2 2,3,4,5,6,7,8,9,10 4,15 1000000'
-	print ''
-	exit()
+args = parser.parse_args()
 
-# Error
-if args_num < 7:
-    print >> sys.stderr, "Error not enought parameters. See --help."
-    exit()
-
-# algorithm
-algs      = sys.argv[1] # algorithms
-# arguments:
-set_args  = sys.argv[2] # test text sets
-r_args    = sys.argv[3] # pattern length
-m_args    = sys.argv[4] # pattern length
-u_args    = sys.argv[5] # U
-k_args    = sys.argv[6] # K
-q_args    = sys.argv[7] # Q
-sig_args  = sys.argv[8] # Sigma
-denom_args= sys.argv[9] # denominator
-
-set_args_list    = set_args.split(',')
-a_args_list      = algs.split(',')
-r_args_list      = r_args.split(',')
-m_args_list      = m_args.split(',')
-u_args_list      = u_args.split(',')
-k_args_list      = k_args.split(',')
-q_list           = q_args.split(',')
-sig_list         = sig_args.split(',')
-denom_args_list  = denom_args.split(',')
-
-print 'Starting calculation for following parameters'
-print 'Sets         : ' + set_args
-print 'M            : ' + m_args
-print 'U            : ' + u_args
-print 'K            : ' + k_args
-print 'Q-Size       : ' + q_args
-print 'Sigma        : ' + sig_args
-print 'Denom        : ' + denom_args
+set_args_list    = args.c.split(',')
+a_args_list      = args.a.split(',')
+r_args_list      = args.r.split(',')
+m_args_list      = args.m.split(',')
+u_args_list      = args.u.split(',')
+k_args_list      = args.k.split(',')
+q_list           = args.q.split(',')
+sig_list         = args.s.split(',')
+denom_args_list  = args.d.split(',')
 
 
-print 'Alg\t' + 'Set\t' + '#Patt\t' + 'Q\t' + 'M\t' + 'U\t' + 'k\t' + 'Sig\t' + 'Sig^Q\t' + 'PattBytes\t' + 'Denom\t' + 'Offset\t' + 'enc_txt_size\t' + 'proc_txt_size\t' + 'dict_size\t' + 'idx_size\t' + 'map_size(RAM)\t' + 'all_space/enc_txt_size\t' + 'all_space/proc_txt_size\t' + '#verif\t' + '#while_loops\t' + 'Matches\t' + 'acc[s]\t' + 'pre[s]\t' + 'search[s]\t' + 'pre+search[s]\t' + 'Search[MB/s]\t' + 'Pre+Search[MB/s]\t' + 'P+S/Patt[MB/s]'
+print('Alg\tSet\t#Patt\tQ\tM\tU\tk\tSig\tSig^Q\tPattBytes\tDenom\tOffset\tenc_txt_size\tproc_txt_size\tdict_size\tidx_size\tmap_size(RAM)\tall_space/enc_txt_size\tall_space/proc_txt_size\t#verif\t#while_loops\tMatches\tacc[s]\tpre[s]\tsearch[s]\tpre+search[s]\tSearch[MB/s]\tPre+Search[MB/s]\tP+S/Patt[MB/s]')
 for a in a_args_list:
 	for sig in sig_list:
 		for q in q_list:
@@ -87,15 +46,15 @@ for a in a_args_list:
 									set_filename     = set_loc + '/' + s + '.processed.etdc.bin'
 									set_orig_filename= set_loc + '/' + s 
 									if not os.path.isfile(proc_filename):
-										print "Error: The prog file not found ( "+proc_filename+")"
+										print("Error: The prog file not found ( "+proc_filename+")")
 										exit()
 									if not os.path.isfile(pattern_filename):
-										print "Error: The pattern file not found ( "+pattern_filename+")"
+										print("Error: The pattern file not found ( "+pattern_filename+")")
 										exit()
 									if not os.path.isfile(set_filename):
-										print "Error: The set file not found ( "+set_filename+")"
+										print("Error: The set file not found ( "+set_filename+")")
 										exit()
-									#print proc_filename, pattern_filename, m, set_filename, u, k, q, sig, denom, set_orig_filename
+									#print(proc_filename, pattern_filename, m, set_filename, u, k, q, sig, denom, set_orig_filename
 									proc = subprocess.Popen([proc_filename, pattern_filename, m, set_filename, u, k, q, sig, denom, set_orig_filename], stdout=subprocess.PIPE)
 									output = proc.stdout.read()
 									output = a + '\t' + s + '\t' + r + '\t' + q + '\t' + m + '\t' + u + '\t' + k + '\t' + output;
